6c494bf2a080f3255f1a70036989372df7bd7a2c
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2023-08-12 10:44:13 +0700

    create-update-delete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,7 +15,7 @@ let handleLogin = async (req, res) => {
   });
 };
 let handleGetAllUsers = async (req, res) => {
-  let id = req.body.id; //ALL,id
+  let id = req.query.id; //ALL,id
   if (!id) {
     return res.status(200).json({
       errCode: 1,
@@ -24,10 +24,38 @@ let handleGetAllUsers = async (req, res) => {
     });
   }
   let users = await userService.getAllUsers(id);
+  // console.log(users);
   return res.status(200).json({
     errCode: 0,
     errMessage: "All user",
     user: users,
   });
 };
-module.exports = { handleLogin, handleGetAllUsers };
+let handleCreateNewUser = async (req, res) => {
+  // let data = req.body;
+  let message = await userService.createNewUser(req.body);
+  return res.status(200).json(message);
+};
+let handleEditUser = async (req, res) => {
+  let data = req.body;
+  let message = await userService.updateUserData(data);
+  return res.status.json(message);
+};
+
+let handleDeleteUser = async (req, res) => {
+  if (!req.body.id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameters",
+    });
+  }
+  let message = await userService.deleteUser(req.body.id);
+  return res.status(200).json(message);
+};
+module.exports = {
+  handleLogin,
+  handleGetAllUsers,
+  handleCreateNewUser,
+  handleEditUser,
+  handleDeleteUser,
+};
--- src/services/userService.js
+++ src/services/userService.js
@@ -93,4 +93,99 @@ let getAllUsers = (userId) => {
     }
   });
 };
-module.exports = { handleUserLogin, checkUserEmail, getAllUsers };
+let createNewUser = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let check = await checkUserEmail(data.email);
+      if (check === true) {
+        resolve({
+          errCode: 1,
+          message: "Your email is already used, try other email",
+        });
+      }
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+      await db.User.create({
+        email: data.email,
+        password: hashPasswordFromBcrypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        phoneNumber: data.phonenumber,
+        gender: data.gender === "1" ? true : false,
+        roleId: data.roleId,
+      });
+      resolve({
+        errCode: 0,
+        message: "ok",
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassword = await bcrypt.hashSync(password, salt);
+      resolve(hashPassword);
+    } catch (e) {
+      reject(e);
+    }
+    // Store hash in your password DB.
+  });
+};
+let deleteUser = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    let user = await db.User.findOne({ where: { id: userId } });
+    if (!user) {
+      resolve({
+        errCode: 2,
+        errMessage: `The user isn't exist`,
+      });
+    }
+    // await user.destroy();
+    await db.User.destroy({ where: { id: userId } });
+    resolve({
+      errCode: 0,
+      message: "The user is deleted",
+    });
+  });
+};
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.id) {
+        resolve({
+          errCode: 2,
+          errMessage: "Missing required parameter",
+        });
+      }
+      let user = await db.User.findOne({ where: { id: data.id }, raw: false });
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+        resolve({
+          errCode: 0,
+          message: "Update the user succeed!",
+        });
+      } else {
+        resolve({
+          errCode: 1,
+          errMessage: `User's not found!`,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+module.exports = {
+  handleUserLogin,
+  checkUserEmail,
+  getAllUsers,
+  createNewUser,
+  deleteUser,
+  updateUserData,
+};
